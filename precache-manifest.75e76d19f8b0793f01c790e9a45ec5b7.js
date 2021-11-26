48b66698c9d4e20c623af349685d0601e7732381
jonahmiller00 <60237947+jonahm2@users.noreply.github.com>
2021-11-26 10:51:42 -0600

    Updates

---
+++ precache-manifest.75e76d19f8b0793f01c790e9a45ec5b7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706d8ddb57333d9367e3c174e842e2f8",
+    "url": "/NCAA-visualizer/index.html"
+  },
+  {
+    "revision": "820c0059db2785bf41e0",
+    "url": "/NCAA-visualizer/static/css/2.d5877b5f.chunk.css"
+  },
+  {
+    "revision": "e13d692ac31a7f465e61",
+    "url": "/NCAA-visualizer/static/css/main.2b0dd170.chunk.css"
+  },
+  {
+    "revision": "820c0059db2785bf41e0",
+    "url": "/NCAA-visualizer/static/js/2.a34a0bac.chunk.js"
+  },
+  {
+    "revision": "466720bd6bb693254a685114f659fca9",
+    "url": "/NCAA-visualizer/static/js/2.a34a0bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be19593e4f3abb5f281e",
+    "url": "/NCAA-visualizer/static/js/3.e6740d79.chunk.js"
+  },
+  {
+    "revision": "e13d692ac31a7f465e61",
+    "url": "/NCAA-visualizer/static/js/main.2e13dc40.chunk.js"
+  },
+  {
+    "revision": "ba11e55295f19229a6ac",
+    "url": "/NCAA-visualizer/static/js/runtime-main.39954644.js"
+  },
+  {
+    "revision": "455fd93686df807d87724b1a3a658613",
+    "url": "/NCAA-visualizer/static/media/old_picks copy 2.455fd936.csv"
+  },
+  {
+    "revision": "da3600abc6aa430fc1716e2222ef45d2",
+    "url": "/NCAA-visualizer/static/media/old_picks copy.da3600ab.csv"
+  },
+  {
+    "revision": "71c301d7d26bf9f0d8aa2511ff56dddc",
+    "url": "/NCAA-visualizer/static/media/open-sans-all-400-normal.71c301d7.woff"
+  },
+  {
+    "revision": "b398ee10078c96cd441d3c13c9503c82",
+    "url": "/NCAA-visualizer/static/media/open-sans-cyrillic-400-normal.b398ee10.woff2"
+  },
+  {
+    "revision": "d341cfd9aae09dcda354545d532219f8",
+    "url": "/NCAA-visualizer/static/media/open-sans-cyrillic-ext-400-normal.d341cfd9.woff2"
+  },
+  {
+    "revision": "9a34971bad7a835120064912496420ee",
+    "url": "/NCAA-visualizer/static/media/open-sans-greek-400-normal.9a34971b.woff2"
+  },
+  {
+    "revision": "fddbc213616906d5a82a33a96a86d8d3",
+    "url": "/NCAA-visualizer/static/media/open-sans-greek-ext-400-normal.fddbc213.woff2"
+  },
+  {
+    "revision": "7d3407f519f410975170bfdda2d2148e",
+    "url": "/NCAA-visualizer/static/media/open-sans-hebrew-400-normal.7d3407f5.woff2"
+  },
+  {
+    "revision": "d65113b6da7ba4bd0a59dbda5a7e24d4",
+    "url": "/NCAA-visualizer/static/media/open-sans-latin-400-normal.d65113b6.woff2"
+  },
+  {
+    "revision": "06ece074e82e8955aa2417f59d80161c",
+    "url": "/NCAA-visualizer/static/media/open-sans-latin-ext-400-normal.06ece074.woff2"
+  },
+  {
+    "revision": "14a711a75d220acbc3518b0f20d376bd",
+    "url": "/NCAA-visualizer/static/media/open-sans-vietnamese-400-normal.14a711a7.woff2"
+  }
+]);
